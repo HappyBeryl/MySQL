f0f971ec6259656e852f36e008002e3c721f96ed
123 <897131608@qq.com>
2020-02-12 20:19:50 +0800

    MySQL的复杂连接

--- Demo.sql
+++ Demo.sql
@@ -80,4 +80,26 @@ INSERT INTO exam_result (id,name, chinese, math, english) VALUES
   
   DELETE FROM 表 WHERE 条件
  
+ --JDBC
+ 
+ CREATE DATABASE csdnusejdbc CHARSET utf8mb4;
+ 
+USE csdnusejdbc;
+ 
+
+CREATE TABLE users (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	username VARCHAR(255) UNIQUE NOT NULL,
+	password VARCHAR(255) NOT NULL
+	);
+ 
+CREATE TABLE articles (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	author_id INT NOT NULL,
+	title VARCHAR(255),
+	content TEXT
+);
+
+DELETE FROM users WHERE id = 1;
+DELETE FROM users WHERE id = 2;
  
